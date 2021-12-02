87bb7c0a4fad76ba823d7b5e362226f98522aab6
Stephen Pike <steviepike@yahoo.co.uk>
2021-12-02 15:12:19 +0000

    Complete day-02 puzzle 2

--- src/day-02.py
+++ src/day-02.py
@@ -1,4 +1,5 @@
 """
+Part 1:
 Now, you need to figure out how to pilot this thing.
 
 It seems like the submarine can take a series of commands like forward 1, 
@@ -37,6 +38,43 @@ and a depth of 10. (Multiplying these together produces 150.)
 Calculate the horizontal position and depth you would have after following the 
 planned course. What do you get if you multiply your final horizontal position 
 by your final depth?
+
+Part 2:
+Based on your calculations, the planned course doesn't seem to make any sense. 
+You find the submarine manual and discover that the process is actually 
+slightly more complicated.
+
+In addition to horizontal position and depth, you'll also need to track a third 
+value, aim, which also starts at 0. The commands also mean something entirely 
+different than you first thought:
+
+    down X increases your aim by X units.
+    up X decreases your aim by X units.
+    forward X does two things:
+        It increases your horizontal position by X units.
+        It increases your depth by your aim multiplied by X.
+
+Again note that since you're on a submarine, down and up do the opposite of 
+what you might expect: "down" means aiming in the positive direction.
+
+Now, the above example does something different:
+
+    - forward 5 adds 5 to your horizontal position, a total of 5. Because your 
+      aim is 0, your depth does not change.
+    - down 5 adds 5 to your aim, resulting in a value of 5.
+    - forward 8 adds 8 to your horizontal position, a total of 13. Because your 
+      aim is 5, your depth increases by 8*5=40.
+    - up 3 decreases your aim by 3, resulting in a value of 2.
+    - down 8 adds 8 to your aim, resulting in a value of 10.
+    - forward 2 adds 2 to your horizontal position, a total of 15. Because your 
+      aim is 10, your depth increases by 2*10=20 to a total of 60.
+
+After following these new instructions, you would have a horizontal position of 
+15 and a depth of 60. (Multiplying these produces 900.)
+
+Using this new interpretation of the commands, calculate the horizontal 
+position and depth you would have after following the planned course. What do 
+you get if you multiply your final horizontal position by your final depth?
 """
 
 import os
@@ -46,19 +84,33 @@ input_path = os.path.join('data', 'day-02_input-1.txt')
 with open(input_path, 'r') as f:
     raw_data = f.readlines()
 
+# Part 1 ----------------------
 # clean raw_data
 movements = [x.replace('\n', '').split() for x in raw_data]
-vert = 0
-horiz = 0
+v, h = 0, 0
 for m in movements:
     direction = m[0]
     dist = int(m[1])
     if direction in ['up', 'backward']:
         dist *= -1
     if direction in ['up', 'down']:
-        vert += dist
+        v += dist
     else:
-        horiz += dist
+        h += dist
 
-print(horiz*vert)
+print("Part 1 answer:", h*v)
 
+# Part 2 ----------------------
+v, h, aim = 0, 0, 0
+for m in movements:
+    direction = m[0]
+    units = int(m[1])
+    if direction in ['up', 'backward']:
+        units *= -1
+    if direction in ['up', 'down']:
+        aim += units
+    else:
+        h += units
+        v += aim * units
+
+print("Part 2 answer:", h*v)
