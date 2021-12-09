396aca4d32d3fa873129bf77db5efcb3e0516321
Stephen Pike <steviepike@yahoo.co.uk>
2021-12-09 23:43:22 +0000

    Complete day-07 puzzles 1 and 2

--- aoc2021.py
+++ aoc2021.py
@@ -4,9 +4,11 @@ Import data for advent of code
 
 import os
 
-def import_data(day, puzzle=1, input_dir = 'data'):
+def import_data(day, puzzle=1, input_dir = 'data', as_list=True):
     input_path = os.path.join(input_dir, f'day-{day:02d}_input-{puzzle}.txt')
     with open(input_path, 'r') as f:
         data = [l.strip() for l in f.readlines()]
 
+    if not as_list:
+        data = ''.join(data)
     return data
